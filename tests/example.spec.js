227d8693e29f0f1e3da4a9b3591d3c04c913058b
sangyeon217 <sangyeon217@gmail.com>
2024-06-17 23:02:14 +0900

    샘플 패키지 버전 업데이트 테스트

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,9 +2,16 @@ const { test, expect } = require("@playwright/test");
 const {
   GooglePage,
 } = require("@sangyeon217/nodejs-sample-package/google-page");
+const { NaverPage } = require("@sangyeon217/nodejs-sample-package/naver-page");
 
 test("Google Page Has Title", async ({ page }) => {
   const googlePage = new GooglePage(page);
   await googlePage.goto();
   await expect(page).toHaveTitle(/Google/);
+});
+
+test("Naver Page Has Title", async ({ page }) => {
+  const naverPage = new NaverPage(page);
+  await naverPage.goto();
+  await expect(page).toHaveTitle(/NAVER/);
 });
