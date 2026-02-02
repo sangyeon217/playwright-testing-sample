d9352784b084068d0874111e9f319e8d57eaeff2
Sangyeon <sangyeon217@gmail.com>
2026-02-02 23:46:00 +0900

    프록시 설정 추가

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,16 @@
-// @ts-check
 const { defineConfig, devices } = require("@playwright/test");
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require("dotenv").config();
+
+const proxyConfig = {
+  server: process.env.PROXY_SERVER,
+  username: process.env.PROXY_USERNAME,
+  password: process.env.PROXY_PASSWORD,
+};
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -26,6 +31,7 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    proxy: process.env.NETWORK === "ABROAD" ? proxyConfig : undefined,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
